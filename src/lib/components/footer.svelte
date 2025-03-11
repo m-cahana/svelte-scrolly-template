d42213706cb545b25ed24a6d8704e2721ea50e0e
Michael Cahana <cahanamichael@gmail.com>
2025-03-10 22:19:00 -0400

    scrolly init

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,30 @@
+<footer>
+    <div class="line"></div>
+    <div class="footer-content">
+        <p>Footer text</p>
+    </div>
+</footer>
+
+<style>
+    footer {
+        padding: 2rem 4rem;
+        margin-top: 2rem;
+    }
+
+    .line {
+        border-top: 1px solid black;
+        width: 100%;
+        margin-bottom: 1rem;
+    }
+
+    .footer-content {
+        font-family: "Helvetica";
+        font-weight: 300;
+        text-align: center;
+    }
+
+    p {
+        font-size: 14px;
+        text-align: left;
+    }
+</style> 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Scrolly from "$lib/components/scrolly.svelte";
+    import Footer from "$lib/components/footer.svelte";
+</script>
+
+<main>
+	<h1>Lorem ipsum dolor sit amet</h1>
+    <div class="subtitle">March 2025</div>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+
+	<Scrolly />
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+
+    <Scrolly />
+</main>
+
+<Footer />
+
+<style>
+	main {
+		margin: 0 auto;
+		max-width: 800px;
+		padding: 1rem;
+        font-family: "Helvetica";
+		text-align: center;
+	}
+
+
+	h1 {
+		font-size: 36px;
+        font-style: italic;
+        font-weight: 300;
+        margin-bottom: 0.2rem;
+	}
+
+    p {
+        font-size: 16px;
+        text-align: left;
+        font-weight: 300;
+    }
+
+    .subtitle {
+        font-size: 20px;
+        font-style: italic;
+        font-weight: 300;
+        margin-top: 0;
+    }
+</style>
